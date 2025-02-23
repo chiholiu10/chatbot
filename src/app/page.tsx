9680db8b58f766ae61be8b64c07dee2c71e45a88
chiho <chiholiu10@gmail.com>
2025-02-23 22:19:15 +0100

    feat: added Chatbot component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import { ThemeProvider } from "styled-components";
 import { CSSreset } from "./styles/CssReset";
 import theme from "./styles/Theme";
+import { Chatbot } from "./components/organism";
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <CSSreset />
+      <Chatbot />
     </ThemeProvider>
   );
 }
