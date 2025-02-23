4caf056ff3fef41e13b92c476c461108e74928fe
chiho <chiholiu10@gmail.com>
2025-02-23 22:00:12 +0100

    commit
    feat: molecule index.ts created

---
+++ src/app/components/molecules/index.ts
@@ -0,0 +1,6 @@
+import ChatbotFooter from "./ChatbotFooter/ChatbotFooter";
+import ChatbotHeader from "./ChatbotHeader/ChatbotHeader";
+import ChatbotInput from "./ChatbotInput/ChatbotInput";
+import ChatbotScreen from "./ChatbotScreen/ChatbotScreen";
+
+export { ChatbotFooter, ChatbotHeader, ChatbotInput, ChatbotScreen };
