0eab158ca04e785b64880edfdbdb87040affcfe9
chiho <chiholiu10@gmail.com>
2025-03-05 21:06:04 +0100

    fix: update chatbot widget

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/widget",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // Allows embedding in iframes
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *", // Allow embedding from any domain
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: false,
 };
 
